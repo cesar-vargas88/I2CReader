9a53f959d0b786270efc73bf39dab48a9d60124c
cesar-vargas88 <vargas.cesar88@gmail.com>
2017-08-08 17:46:14 -0500

    I2C reader ready.

---
+++ I2C_C_digo.ino
@@ -0,0 +1,62 @@
+
+#include <Wire.h>
+
+void setup() {
+  Serial.begin (115200);
+
+  // Leonardo: wait for serial port to connect
+  while (!Serial) 
+    {
+    }
+
+  Serial.println ();
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+  
+  Wire.begin();
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+      {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+      } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+}  // end of setup
+
+void loop() {}
+
+
+/*
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup()
+{
+  // initialize the LCD
+  lcd.begin();
+
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+}
+
+void loop()
+{
+  lcd.print("Hello");
+  delay(1000);
+}
+*/
